e11b6d5e6e54dadff63cb7e485b4e7cd5a7ea590
Génesis Morales <genesismoralesc1@gmail.com>
2026-03-07 16:00:05 -0600

    Update AppRouter.tsx

--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -1,12 +1,8 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Spin } from "antd";
 
 const Home = lazy(() => import("../features/auth/pages/home/Home"));
-//const Login = lazy(() => import("../pages/Login/Login"));
-//const Register = lazy(() => import("../pages/Register/Register"));
-//const PasswordReset = lazy(() => import("../pages/PasswordReset/PasswordReset"));
-//const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard"));
 
 const LoadingFallback: React.FC = () => (
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
